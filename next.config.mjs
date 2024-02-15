405f3f3f5c5dd907de49ff5a94c0d0aacb4ddbe0
realvjy <realvjy@gmail.com>
2024-02-15 20:16:42 +0530

    cors update

--- next.config.mjs
+++ next.config.mjs
@@ -31,7 +31,7 @@ const nextConfig = {
         return [
             {
                 // matching all API routes
-                source: "/uilogos/:path*",
+                source: "/:path*",
                 headers: [
                     { key: "Access-Control-Allow-Credentials", value: "true" },
                     { key: "Access-Control-Allow-Origin", value: "*" },
