f98c74d301945a5ec5076b1d4d76b7a2b76c1f47
realvjy <realvjy@gmail.com>
2024-02-15 19:03:54 +0530

    set redirect

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,30 @@ const nextConfig = {
     compiler: {
         styledComponents: true,
     },
+    async redirects() {
+        return [
+            {
+                source: '/(figma|plugin|f)',
+                destination: 'https://www.figma.com/community/plugin/1120667703468196103/uilogos',
+                permanent: true,
+            },
+            {
+                source: '/sketch',
+                destination: 'https://github.com/realvjy/uiLogos-sketch-plugin',
+                permanent: true,
+            },
+            {
+                source: '/xd',
+                destination: 'https://github.com/realvjy/uilogos-XD',
+                permanent: true,
+            },
+            {
+                source: '/(author|realvjy|v)',
+                destination: 'https://vjy.me',
+                permanent: true,
+            },
+        ];
+    },
 };
 
 export default nextConfig;
